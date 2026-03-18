aad75a307da3571aace13e572e14e9dba8214fe0
Elavazhagan <elavazhaganrajakumar@gmail.com>
2026-03-18 16:02:11 +0530

    Initial swift  UIKit project setup

---
+++ CloudCast/CloudCastTests/CloudCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloudCastTests.swift
+//  CloudCastTests
+//
+//  Created by Elavazhagan on 18/03/26.
+//
+
+import Testing
+@testable import CloudCast
+
+struct CloudCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
